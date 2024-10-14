8a600d38cf86db396483a6b3b2b15f1cef00009f
JadsonPS <jadsonps12345@gmail.com>
2024-10-13 22:37:05 -0300

    Adição de tela de login

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let login_ligar= document.getElementById("login");
+login_ligar.addEventListener("click", ()=>{
+    let telaLogin = document.getElementById("login-caixa").style = 'display:flex;'
+    
+})
+
+
+let login_desligar = document.getElementById("close");
+login_desligar.addEventListener("click", ()=>{
+    let telaLogin = document.getElementById("login-caixa").style = 'display:none;'
+    
+})
